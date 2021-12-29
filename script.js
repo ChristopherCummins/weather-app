9e261e5d73466044ecc25be355b071daeece056e
Christopher Cummins <christopher.francis.cummins@gmail.com>
2021-12-29 13:59:08 -0500

    bottom container established

--- script.js
+++ script.js
@@ -3,27 +3,18 @@ function readLocation() {
     return searchInput;
 }
 
-async function getLatitude(city) {
+async function getCityInfo(city) {
     let key = "459b582a130dd8242e0ba23f2fd860ea";
-    let url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + key
+    let url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + key;
     const response = await fetch (url, {mode: "cors"});
     const cityData = await response.json();
-    let latitude = cityData[0].lat;
-    return latitude;
+    console.log(cityData);
+    return cityData;
 }
 
-async function getLongitude(city) {
-    let key = "459b582a130dd8242e0ba23f2fd860ea";
-    let url = "http://api.openweathermap.org/geo/1.0/direct?q=" + city + "&limit=1&appid=" + key
-    const response = await fetch (url, {mode: "cors"});
-    const cityData = await response.json();
-    let longitude = cityData[0].lon;
-    return longitude;
-}
-
-async function getWeatherData() {
-    const longitude = await getLongitude("london");
-    const latitude = await getLatitude("london");
+async function getWeatherData(cityData) {
+    const longitude = cityData[0].lon;
+    const latitude = cityData[0].lat;
     let key = "459b582a130dd8242e0ba23f2fd860ea";
     let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial" + "&appid=" + key
     const response = await fetch (url, {mode: "cors"});
@@ -31,46 +22,45 @@ async function getWeatherData() {
     console.log(weatherData);
     return weatherData;
 }
-
-async function displayNewWeatherData() {
-    const longitude = await getLongitude(readLocation());
-    const latitude = await getLatitude(readLocation());
-    let key = "459b582a130dd8242e0ba23f2fd860ea";
-    let url = "https://api.openweathermap.org/data/2.5/onecall?lat=" + latitude + "&lon=" + longitude + "&units=imperial" + "&appid=" + key
-    const response = await fetch (url, {mode: "cors"});
-    const weatherData = await response.json();
-    console.log(weatherData);
-    return weatherData;
-}
-
 
 async function parseWeatherData() {
-    const weatherData = await displayNewWeatherData();
+    const cityData = await(getCityInfo(readLocation()));
+    const weatherData = await getWeatherData(cityData);
 
+    let domCityName = document.getElementsByClassName("cityName")[0];
     let domCurrentTemp = document.getElementsByClassName("temperature")[0];
+    let domTodaysLow = document.getElementsByClassName("todaysLow")[0];
+    let domTodaysHigh = document.getElementsByClassName("todaysHigh")[0];
     let domCurrentFeelsLike = document.getElementsByClassName("rightDescription")[0];
     let domCurrentHumidity = document.getElementsByClassName("rightDescription")[1];
-    let domTodaysLow = document.getElementsByClassName("todaysLow");
-    let domTodaysHigh = document.getElementsByClassName("todaysHigh");
     let domChanceOfRain = document.getElementsByClassName("rightDescription")[2];
+    let domSunsetTime = document.getElementsByClassName("rightDescription")[3];
     let domPhaseOfMoon = document.getElementsByClassName("rightDescription")[4];
 
+    let cityName = cityData[0].name;
     let currentTemp = Math.round(weatherData.current.temp);
-    let currentFeelsLike = Math.round(weatherData.current.feels_like);
-    let currentHumidity = Math.round(weatherData.current.humidity);
     let todaysLow = Math.round(weatherData.daily[0].temp.min);
     let todaysHigh = Math.round(weatherData.daily[0].temp.max);
+    let currentFeelsLike = Math.round(weatherData.current.feels_like);
+    let currentHumidity = Math.round(weatherData.current.humidity);
     let chanceOfRain = ((weatherData.daily[0].pop) * 100);
+    let sunsetTime = weatherData.current.sunset;
     let phaseOfMoon = (weatherData.daily[0].moon_phase);
-    //let todaysSunset = new Date (weatherData.current.sunset).toLocaleTimeString("en-US");
+    let currentDate = weatherData.current.dt;
 
+
+    domCityName.innerHTML = cityName;
     domCurrentTemp.innerHTML = currentTemp + "&#176;";
+    domTodaysLow.innerHTML = "Low: " + todaysLow + "&#176;";
+    domTodaysHigh.innerHTML = "High: " + todaysHigh + "&#176;";
     domCurrentFeelsLike.innerHTML = currentFeelsLike + "&#176;";
     domCurrentHumidity.innerHTML = currentHumidity + " %"
-    domTodaysLow.innerHTML = todaysLow + "&#176;";
-    domTodaysHigh.innerHTML = todaysHigh + "&#176;";
     domChanceOfRain.innerHTML = chanceOfRain + " %";
+    domSunsetTime.innerHTML = getSunsetTime(sunsetTime);
     domPhaseOfMoon.innerHTML = getMoonPhase(phaseOfMoon);
+
+    console.log(getCurrentDayofWeek(currentDate));
+    console.log(convertDayofWeek(getCurrentDayofWeek(currentDate)));
 
     console.log("Current Temp: " + currentTemp, 
                 "Current Humidity: " + currentHumidity, 
@@ -78,7 +68,6 @@ async function parseWeatherData() {
                 "Today's High: " + todaysHigh,
                 "Phase of Moon: " + phaseOfMoon);
 }
-
 
 function getMoonPhase(number) {
         if ((number == 0) || (number == 1)) {
@@ -107,5 +96,30 @@ function getMoonPhase(number) {
         }
 }
 
-getWeatherData();
+function getSunsetTime(unixTime) {
+    let date = new Date(unixTime*1000);
+    let hours = date.getHours();
+    let minutes = date.getMinutes();
+    if (hours > 12) {
+        hours = hours - 12;
+    }
+    let sunsetTime = hours + ":" + minutes + " pm";
+    return sunsetTime;
+}
 
+function getCurrentDayofWeek(unixTime) {
+    let date = new Date(unixTime * 1000);
+    let currentDayOfWeek = date.getDay();
+    return currentDayOfWeek;
+}
+
+function convertDayofWeek(currentDayOfWeek) {
+    const weekDays = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"];
+    let day = weekDays[currentDayOfWeek];
+    console.log(day);
+    return day;
+}
+
+function fillWeeklyDaysOfWeek(weatherData) {
+    let domDaysOfWeek = document.getElementsByClassName("date");
+}
